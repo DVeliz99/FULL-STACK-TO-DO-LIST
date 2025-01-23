5aad08a95060f2b2361c0b4db74b6a52b989a530
Dario Veliz <velizalexander99@gmail.com>
2025-01-23 15:09:39 -0600

    Configured for production

--- task_manager/src/environment/environment.ts
+++ task_manager/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    apiUrl: 'http://localhost/mi-proyecto/crud/', // URL base para las operaciones CRUD
-  };
+  production: true,
+  apiUrl: 'http://localhost/mi-proyecto/crud/', // URL base para las operaciones CRUD
+};
 
 
-  
